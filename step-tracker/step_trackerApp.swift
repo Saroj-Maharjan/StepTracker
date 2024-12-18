cd5cbeefb4c9c664c7415820c959809eef5c6bf2
saroj maharjan <saroj.maharjan1993@gmail.com>
2024-12-18 19:26:09 +0800

    Initial Commit

---
+++ step-tracker/step_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  step_trackerApp.swift
+//  step-tracker
+//
+//  Created by saroj maharjan on 18/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct step_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ step-trackerTests/step_trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  step_trackerTests.swift
+//  step-trackerTests
+//
+//  Created by saroj maharjan on 18/12/2024.
+//
+
+import Testing
+@testable import step_tracker
+
+struct step_trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
