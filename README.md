15b16ed4673c2d54986967f8a3a56145c69cef7a
saroj maharjan <saroj.maharjan1993@gmail.com>
2025-02-11 19:07:34 +0800

    Adding Readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Step Tracker
+Step tracker intregrate Apple Health to show latest steps and weight in animated, interactive swift charts.
+
+Step Tracker also allows you to upload new step or weight data to the Apple Health app.
+
+# Technology Used
+* Swift UI
+* HealthKit
+* Swift Charts
+* Swift Algorithms
+* DocC
+* Git & github
+
+# Completeness
+Although it's a simple portfolio project, I've implemented the following
+* Error handling & alerts
+* Empty states
+* Permission Priming
+* Text input validation
+* Basic unit tests
+* Basic accessibility
+* Privacy Manifest
+* Code documentation (DocC)
+* Project organization
--- step-trackerTests/step_trackerTests.swift
+++ step-trackerTests/step_trackerTests.swift
@@ -6,12 +6,38 @@
 //
 
 import Testing
+import Foundation
 @testable import step_tracker
 
 struct step_trackerTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    
+    @Test func dateValueAverage() {
+        let data: [DateValueChartData] = [
+            .init(date: .now, value: 5.1),
+            .init(date: .now, value: 10.54),
+            .init(date: .now, value: 15.11)
+        ]
+        
+        #expect(data.average == 10.25)
     }
+}
 
+@Suite("HealthMetric Extensions Tests") struct HealthMetricExtensionsTests {
+    
+    let metrics: [HealthMetric] = [
+        .init(date: Calendar.current.date(from: .init(year: 2024, month: 10, day: 14)) ?? .now, value: 100), // Mon
+        .init(date: Calendar.current.date(from: .init(year: 2024, month: 10, day: 15)) ?? .now, value: 200), // Tue
+        .init(date: Calendar.current.date(from: .init(year: 2024, month: 10, day: 16)) ?? .now, value: 300), // Wed
+        .init(date: Calendar.current.date(from: .init(year: 2024, month: 10, day: 21)) ?? .now, value: 500), // Mon
+        .init(date: Calendar.current.date(from: .init(year: 2024, month: 10, day: 22)) ?? .now, value: 600), // Tue
+    ]
+    
+    @Test func averageWeekdayCount() {
+        let averageWeekdayCount = metrics.averageWeekDayCountData
+        
+        #expect(averageWeekdayCount.count == 3) // How many different days?
+        #expect(averageWeekdayCount[0].value == 300) // Monday avg
+        #expect(averageWeekdayCount[1].value == 400) // Tuesday avg
+        #expect(averageWeekdayCount[2].date.weekdayTitle == "Wednesday") // Wed title
+    }
 }
